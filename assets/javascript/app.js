c77f0e2271b76f971e9457e3ef78b87025cc0e73
hishamss <hishamss90@gmail.com>
2020-02-27 09:45:12 -0700

    created question container

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,11 @@ $(document).ready(function() {
   window.addEventListener("resize", reposition);
   function reposition() {
     var ContHeight = $(".bg").height();
-    var CalculatedContHeight = ContHeight * 0.5 - 233;
+    var GameHeight = $(".game").height();
+    var WelcomeHeight = $(".welcome").height();
+    var CalculatedContHeight = ContHeight * 0.5 - WelcomeHeight / 2;
+    var CalculatedGameHeight = ContHeight * 0.5 - GameHeight / 2;
     $(".welcome").css("margin-top", CalculatedContHeight);
+    $(".game").css("margin-top", CalculatedGameHeight);
   }
 });
