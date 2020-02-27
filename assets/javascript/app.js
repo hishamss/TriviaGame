44b2cb98b77fe3fa22ed0a63163bced2f6c22821
hishamss <hishamss90@gmail.com>
2020-02-27 15:11:26 -0700

    now the game container display each question for 3 sec

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,21 @@
+var questions = [
+  "How many legs does a spider have?",
+  "What is the color of an emerald?",
+  "What’s the name of a place you go to see lots of animals?",
+  "What colors are the stars on the American flag?",
+  "Which ocean is off the California coast?"
+];
+var options = [
+  ["Eight", "Seven", "Six", "Five"],
+  ["Blue", "Red", "Green", "Black"],
+  ["Airport", "Zoo", "Park", "Lake"],
+  ["Red", "Blue", "Black", "White"],
+  ["The Pacific", "The Atlantic", "The Indian", "The Arctic"]
+];
+var answers = [options[0], options[2], options[1], options[3], options[0]];
+var time_sec = 35;
+var count = 0;
+var IntervaID;
 $(document).ready(function() {
   reposition();
   $(".game").hide();
@@ -13,6 +31,39 @@ $(document).ready(function() {
   }
   $("#start").click(function() {
     $(".welcome").hide();
+    DisplayQuestion();
     $(".game").show();
+    StartGame();
   });
+
+  function StartGame() {
+    IntervaID = setInterval(function() {
+      DisplayQuestion();
+    }, 3000);
+  }
+
+  function DisplayQuestion() {
+    console.log("hit");
+    $(".questions").html(questions[count]);
+
+    for (i = 0; i < 4; i++) {
+      $("#" + i).html(options[count][i]);
+    }
+    count++;
+    if (count == questions.length) {
+      clearInterval(IntervaID);
+    }
+  }
+
+  // function timer() {
+  //   time_sec--;
+  //   $("#sec").html(time_sec);
+  // }
+  // function sleep(milliseconds) {
+  //   var date = Date.now();
+  //   var currentDate = null;
+  //   do {
+  //     currentDate = Date.now();
+  //   } while (currentDate - date < milliseconds);
+  // }
 });
