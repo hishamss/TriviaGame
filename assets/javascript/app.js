0fb2040520c53424cb139ba357b1961fb5d6200a
hishamss <hishamss90@gmail.com>
2020-02-27 23:27:16 -0700

    the game displays the results at the end of the game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,8 +21,10 @@ var answers = [
 ];
 var time_sec = 20;
 var count = 0;
-var DisplayIntervaID, TimerIntervaID;
-var TimerCount = 1;
+var TimerIntervaID;
+var Correct = 0,
+  Incorrect = 0,
+  Unanswered = 0;
 $(document).ready(function() {
   reposition();
   $(".game").hide();
@@ -31,37 +33,35 @@ $(document).ready(function() {
   function reposition() {
     var ContHeight = $(".bg").height();
     var GameHeight = $(".game").height();
+    var ResultHeight = $(".result").height();
     var WelcomeHeight = $(".welcome").height();
     var CalculatedContHeight = ContHeight * 0.5 - WelcomeHeight / 2;
     var CalculatedGameHeight = ContHeight * 0.5 - GameHeight / 2;
+    var CalculatedResultHeight = ContHeight * 0.5 - ResultHeight / 2;
     $(".welcome").css("margin-top", CalculatedContHeight);
     $(".game").css("margin-top", CalculatedGameHeight);
+    $(".result").css("margin-top", CalculatedResultHeight);
   }
   $("#start").click(function() {
     $(".welcome").hide();
     // to display the first question after you click start button
     DisplayQuestion();
-    // StartGame();
   });
 
-  function StartGame() {
-    // DisplayIntervaID = setInterval(function() {
-    //   DisplayQuestion();
-    // }, 20000);
-    // TimerIntervaID = setInterval(function() {
-    //   timer();
-    // }, 1000);
-  }
   function DisplayQuestion() {
     // when we get the last question
     if (count == questions.length) {
       clearInterval(TimerIntervaID);
       $(".game").hide();
-      $(".result").text("End of Game");
+      $(".result").html("<p>Correct Answers: " + Correct + "</p>");
+      $(".result").append("<p>Incorrect Answers: " + Incorrect + "</p>");
+      $(".result").append("<p>Unanswered: " + Unanswered + "</p>");
       $(".result").show();
+      // exit the function after all questions have been displayed
       return;
     }
     time_sec = 20;
+    $("#sec").html(time_sec);
     TimerIntervaID = setInterval(function() {
       timer();
     }, 1000);
@@ -69,6 +69,7 @@ $(document).ready(function() {
     for (i = 0; i < 4; i++) {
       $("#" + i).html(options[count][i]);
     }
+    $("#score").html(Correct);
     $(".game").show();
     count++;
   }
@@ -78,8 +79,10 @@ $(document).ready(function() {
     $(".game").hide();
     if ($(this).text() === answers[count - 1]) {
       $(".result").text("Great!");
+      Correct++;
     } else {
       $(".result").text("Hard Luck!");
+      Incorrect++;
     }
     $(".result").show();
     setTimeout(function() {
@@ -89,13 +92,11 @@ $(document).ready(function() {
   });
 
   function timer() {
-    // if (TimerCount == 5) {
-    //   clearInterval(TimerIntervaID);
-    // }
-
     $("#sec").html(time_sec);
     time_sec--;
+    // Timeout
     if (time_sec < 0) {
+      Unanswered++;
       clearInterval(TimerIntervaID);
       $(".game").hide();
       $(".result").text("Time Out");
